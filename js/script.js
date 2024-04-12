425132c105ef97809cdf74a6124afd31e5c639ca
Dackefrsik <danielfrisk21@gmail.com>
2024-04-12 13:48:06 +0200

    Färdig timmer

--- js/script.js
+++ js/script.js
@@ -13,4 +13,41 @@ window.addEventListener("load", () => {
         let infoRef = document.querySelector("#info");
         infoRef.innerHTML = "";
     })
+
+    let timer;
+
+    /*Globaltobjekt som håller värden för timer som uppdaterar värdet i
+    bokningsformulärets timer*/
+    window.oGlobalobject = {
+        currentDate : new Date(),
+        houers : new Date().getHours(),
+        min : new Date().getMinutes(),
+        sec : new Date().getSeconds()
+    }
+ 
+    timer = setInterval(() => {
+        UpTime(oGlobalobject.houers, oGlobalobject.min, oGlobalobject.sec)
+    }, 1000);
+    
 })
+
+function UpTime(houers, min, sec){
+    console.log("Timer");
+    sec += 1;
+    if(sec == 60){
+        min += 1;
+        sec = 0;
+    }
+    else if(min == 60){
+        min = 0;
+        houers += 1;
+    }
+    else if(houers == 24){
+        houers = 0;
+    }
+
+    let timRef = document.querySelector("#Time");
+    timRef.value = houers + ":" + min;
+
+   return oGlobalobject.houers = houers, oGlobalobject.min = min, oGlobalobject.sec = sec;
+}
