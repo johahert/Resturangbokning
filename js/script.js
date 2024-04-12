5876c1689fd8efa43f9a644209dcecffae7810c1
Dackefrsik <danielfrisk21@gmail.com>
2024-04-12 15:41:25 +0200

    Modal för att avboka bord
    Har gjort en exempelmodal för att avbryta bokningar

--- js/script.js
+++ js/script.js
@@ -3,36 +3,64 @@
 window.addEventListener("load", () => {
 
     let otherInfoRef = document.querySelector("#otherInfo");
+    let otherInfoRemoveRef = document.querySelector("#otherInfoRemove");
 
     otherInfoRef.addEventListener("mouseover", () => {
         let infoRef = document.querySelector("#info");
         infoRef.innerHTML = "Alergier, specialkost m.m";
-    })
+    });
 
     otherInfoRef.addEventListener("mouseleave", () => {
         let infoRef = document.querySelector("#info");
         infoRef.innerHTML = "";
-    })
+    });
 
-    let timer;
+    otherInfoRemoveRef.addEventListener("mouseover", () => {
+        let infoRef = document.querySelector("#infoRemove");
+        infoRef.innerHTML = "Alergier, specialkost m.m";
+    });
+
+    otherInfoRemoveRef.addEventListener("mouseleave", () => {
+        let infoRef = document.querySelector("#infoRemove");
+        infoRef.innerHTML = "";
+    });
+
+    let formAvbokaRef = document.querySelector("#avbokaForm");
+    let containerRemoveRef = document.querySelector("#containerRemove");
+
+    formAvbokaRef.addEventListener("submit", (event) => {
+        event.preventDefault();
+        console.log("submit");
+       let searchNameRef = document.querySelector("#searchName");
+
+       if(searchNameRef.value === "Daniel"){
+        console.log("Hej Daniel!");
+        containerRemoveRef.classList.remove("d-none");
+       }
+       else{
+        containerRemoveRef.classList.add("d-none");
+        alert("Hittade inga bokningar")
+
+       }
+    });
 
     /*Globaltobjekt som håller värden för timer som uppdaterar värdet i
     bokningsformulärets timer*/
     window.oGlobalobject = {
-        currentDate : new Date(),
+        timer : null,
         houers : new Date().getHours(),
         min : new Date().getMinutes(),
         sec : new Date().getSeconds()
     }
- 
-    timer = setInterval(() => {
+
+    oGlobalobject.timer = setInterval(() => {
         UpTime(oGlobalobject.houers, oGlobalobject.min, oGlobalobject.sec)
     }, 1000);
     
 })
 
+//Funktion som uppdaterar klockslaget varje secund när timern körs
 function UpTime(houers, min, sec){
-    console.log("Timer");
     sec += 1;
     if(sec == 60){
         min += 1;
