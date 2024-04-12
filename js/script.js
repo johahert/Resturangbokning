4f89448e05067354841c3d4c4854a4c76f4d7489
Dackefrsik <danielfrisk21@gmail.com>
2024-04-12 12:57:50 +0200

    Försök till snygg hover
    Försökt lägga till en hover effekt på frågetecknet

--- js/script.js
+++ js/script.js
@@ -5,6 +5,12 @@ window.addEventListener("load", () => {
     let otherInfoRef = document.querySelector("#otherInfo");
 
     otherInfoRef.addEventListener("mouseover", () => {
-        
+        let infoRef = document.querySelector("#info");
+        infoRef.innerHTML = "Alergier, specialkost med mera";
+    })
+
+    otherInfoRef.addEventListener("mouseleave", () => {
+        let infoRef = document.querySelector("#info");
+        infoRef.innerHTML = "";
     })
 })
